76e8e456639cfec0e399efaf2985d68b09c54304
LenaVeselova <lena.veselova88@gmail.com>
2020-04-08 21:09:37 +0300

    Updates

---
+++ precache-manifest.cb88cc78a7de030f00d2f739c43b896d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "58b4a321dbe24c5be93d",
+    "url": "/react-way-of-samurai-lena/static/css/main.65308e67.chunk.css"
+  },
+  {
+    "revision": "58b4a321dbe24c5be93d",
+    "url": "/react-way-of-samurai-lena/static/js/main.58b4a321.chunk.js"
+  },
+  {
+    "revision": "a6cf19082155502acafe",
+    "url": "/react-way-of-samurai-lena/static/js/1.a6cf1908.chunk.js"
+  },
+  {
+    "revision": "fe4c87bba0ef09e6109a",
+    "url": "/react-way-of-samurai-lena/static/js/runtime~main.fe4c87bb.js"
+  },
+  {
+    "revision": "7af32bfed8573c7f139ee43347c1ff9a",
+    "url": "/react-way-of-samurai-lena/index.html"
+  }
+];
